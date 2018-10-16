6b8caca46e4fdb1ce3268b536dc84d54c316734d
doctord1 <icycharly@gmail.com>
2018-10-16 14:10:25 +0100

    Default route set

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #~ get '/', to: 'contacts#index', as: :home
-  root to: 'users#authenticate'
+  root to: 'users#new'
   scope 'api' do
 
 
